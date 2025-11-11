23d45b30fa6a5a81b2f2c525ab90f6a65c41dc8e
Abdurahman <eabdurahman8@gmail.com>
2025-11-11 08:33:37 +0300

    auth

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,10 @@
 import express from "express";
-// import { toNodeHandler } from "better-auth/node";
-// import { auth } from "./lib/auth";
+
 import cors from "cors";
-// import { errorHandler } from "./middlewares/errorHandler";
-// import { verifyUser } from "./middlewares/auth.middleware";
-// import { companyRouter } from "./routes/company.route";
-// import { internRouter } from "./routes/intern.route";
-// import { job } from "./routes/job.route";
-// import { application } from "./routes/application.route";
-// import { interview } from "./routes/interview.route";
-// import path from "path";
+
 import dotenv from "dotenv";
-// import review from "./routes/review.route";
-// import router from "./routes/chat-app/chat.routes";
+import authRoutes from "./modules/auth/auth.route";
+import { errorHandler } from "./middlewares/errorHandler";
 
 dotenv.config();
 
@@ -21,31 +13,13 @@ const app = express();
 app.use(
   cors()
 );
-// app.all("/api/auth/{*any}", toNodeHandler(auth));
-// app.use(express.json());
 
-// //related to job
-// app.use("/api/jobs", job);
-
-// //related to application
-// app.use("/api/applications", application);
-
-// //related to interviews
-// app.use("/api/interviews", interview);
-
-// //related to interviews
-// app.use("/api/reviews", review);
-
-// //related to chat 
-// app.use("/api/chat", router);
+app.use(express.json());
 
 
-// //multiform registration
-// app.use("/api/company", companyRouter);
-// app.use("/api/intern", internRouter);
+app.use("/api/auth", authRoutes);
 
-// // app.use("/static", express.static(path.join(__dirname, "public")));
-// app.use("/static", express.static(path.join(process.cwd(), "uploads")));
 
-// app.use(errorHandler);
+
+app.use(errorHandler);
 export default app;
